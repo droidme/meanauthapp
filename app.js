4d2501b590b933f01f8e41c3515eb157b41ef610
droidme <reinhard@merfeld.info>
2018-05-14 11:03:57 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('okay'));
+
+app.listen(port, ()=>console.log('listening on port ' + port));
+
+
